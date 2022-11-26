74658775ae9bd02fa8d8cf238962f2d38dd9ea99
Afiya2711 <113673532+Afiya2711@users.noreply.github.com>
2022-11-26 23:44:37 +0530

    Create alert box.js

---
+++ alert box.js
@@ -0,0 +1,7 @@
+let person = prompt("Please enter your name", " ");
+let text;
+if (person == null || person == "") {
+  text = "User cancelled the prompt.";
+} else {
+  text = "Hello " + person + "! How are you today?";
+}
